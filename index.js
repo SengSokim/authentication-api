89e044cb6667897cac6403c6ec91fb62bfdcf466
Sokim <sengsokim88@gmail.com>
2023-08-01 21:24:16 +0700

    trust-proxy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app = express()
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
-
+app.set("trust proxy", 1);
 app.use(cors({
     credentials: true,
     origin: ['http://localhost:3001', 'https://twittorr.netlify.app']
